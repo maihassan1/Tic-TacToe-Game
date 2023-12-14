af2856fb3fa1b316d8e8c464c6fbbd5726d751f2
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2023-12-14 15:01:17 +0200

    Update BoardGame.h

--- Task 2/BoardGame.h
+++ Task 2/BoardGame.h
@@ -21,7 +21,7 @@ public:
     // Returns true if there is any winner
     // either X or O
     // Written in a complex way. DO NOT DO LIKE THIS.
-    virtual bool is_winner() = 0;
+    virtual bool is_winner(int c) = 0;
     // Return true if all moves are done and no winner
     virtual bool is_draw() = 0;
     // Display the board and the pieces on it
@@ -38,7 +38,7 @@ public:
     X_O_Board ();
     bool update_board (int x, int y, char mark);
     void display_board();
-    bool is_winner();
+    bool is_winner(int c);
     bool is_draw();
     bool game_is_over();
 };
